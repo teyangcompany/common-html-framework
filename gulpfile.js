f4e70ffce3da288abad7c731c300402c137e92b8
TY-089\Administrator <lmw6412036@sina.com>
2017-07-20 17:01:08 +0800

    modified:   gulpfile.js modified:   package.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
 var path = require("path")
 var gulp = require("gulp")
 var sass = require("gulp-sass")
+var del = require("del")
 var fileinclude = require("gulp-file-include")
 
 var projectMap = require('gulp-project-map');
@@ -11,51 +12,53 @@ var projectMap = require('gulp-project-map');
 var browserSync = require("browser-sync")
 var reload = browserSync.reload;
 
+gulp.task("clean", function () {
+    return del(["./build/**/*"])
+})
 
 gulp.task("sass", function () {
-    gulp.src("./src/app/style.scss")
+    return gulp.src("./src/app/style.scss")
         .pipe(sass())
         .pipe(gulp.dest("./build/css/"))
 })
 
 gulp.task("html", function () {
-    gulp.src(["./src/app/**/*.html"])
+    return gulp.src(["./src/app/**/*.html"])
         .pipe(fileinclude({
             prefix: '@@',
             basepath: path.join(__dirname, "./src"),
-            context:{
-                web:"//localhost:8090"
+            context: {
+                web: "//localhost:8090"
             }
         }))
         .pipe(gulp.dest("./build/html"))
 })
 
 gulp.task("copy", function () {
-    gulp.src("./src/static/**/*")
+    return gulp.src("./src/static/**/*")
         .pipe(gulp.dest("./build/static"))
 
 })
 
-gulp.task("map",function () {
+gulp.task("map", function () {
     projectMap({
         path: './build',
-        name:"index"
+        name: "index"
     });
 })
 
-gulp.task("default", ['sass', 'html'], function () {
+gulp.task("default", ["clean", "copy", 'sass', 'html'], function () {
     browserSync.init({
-        server:"./build",
-        port:8090,
-        open:"local",
+        server: "./build",
+        port: 8090,
+        open: "local",
         browser: "google chrome"
     })
 
 
     gulp.watch("./src/**/*.scss", ['sass'])
     gulp.watch("./src/**/*.html", ['html'])
-
-    gulp.watch("./build/**/*").on("change",reload)
+    gulp.watch("./build/**/*").on("change", reload)
 })
 
 
